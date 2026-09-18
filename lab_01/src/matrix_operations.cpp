8fc05fe4eb7904449c624c27178febdbaed65ba0
Дубчак Александр Александрович <dubchakaleksandar@yandex.ru>
2026-09-18 19:32:55 +0300

    change a code style

--- lab_01/src/matrix_operations.cpp
+++ lab_01/src/matrix_operations.cpp
@@ -1,61 +1,65 @@
+#include "../include/main.h"
 #include <cstddef>
 #include <iostream>
-#include "../include/main.h"
-
 
 aom *initialize_matrices() {
-    aom *matr = new aom;
-    int ***array = new int **[100000]{};
-    std::size_t *r = new size_t[100000]{};
-    std::size_t *c = new size_t[100000]{};
-    matr->array = array;
-    matr->rows = r;
-    matr->cols = c;
-    return matr;
+  aom *matr = new aom;
+  int ***array = new int **[100000]{};
+  std::size_t *r = new size_t[100000]{};
+  std::size_t *c = new size_t[100000]{};
+  matr->array = array;
+  matr->rows = r;
+  matr->cols = c;
+  return matr;
 }
 
 int **matrix_create(std::size_t rows, std::size_t cols) {
-    int **matrix = new int *[rows];
-    for (std::size_t i = 0; i < rows; i++) {
-        matrix[i] = new int[cols];
-    }
-    return matrix;
+  int **matrix = new int *[rows];
+  for (std::size_t i = 0; i < rows; i++) {
+    matrix[i] = new int[cols];
+  }
+  return matrix;
 }
 
-
 void matrix_delete(int **m, std::size_t rows) {
-    if (m == nullptr) return;
-    for (std::size_t i{}; i < rows; i++) {
-        delete[] m[i];
-        m[i] = nullptr;
-    }
-    delete[] m;
+  if (m == nullptr)
+    return;
+  for (std::size_t i{}; i < rows; i++) {
+    delete[] m[i];
+    m[i] = nullptr;
+  }
+  delete[] m;
 }
 
 void matrix_fill(int **m, std::size_t rows, std::size_t cols, int value) {
-    if (m == nullptr) return;
-    for (std::size_t i = 0; i < rows; i++) {
-        for (std::size_t j = 0; j < cols; j++) {
-            m[i][j] = value;
-        }
+  if (m == nullptr)
+    return;
+  for (std::size_t i = 0; i < rows; i++) {
+    for (std::size_t j = 0; j < cols; j++) {
+      m[i][j] = value;
     }
+  }
 }
 
-void matrix_print(const int *const*m, std::size_t rows, std::size_t cols) {
-    if (m == nullptr) return;
-    std::cout << '\n';
-    for (std::size_t i{}; i < rows; i++) {
-        for (std::size_t j{}; j < cols; j++) {
-            std::cout << m[i][j] << ' ';
-        }
-        std::cout << '\n';
+void matrix_print(const int *const *m, std::size_t rows, std::size_t cols) {
+  if (m == nullptr)
+    return;
+  std::cout << '\n';
+  for (std::size_t i{}; i < rows; i++) {
+    for (std::size_t j{}; j < cols; j++) {
+      std::cout << m[i][j] << ' ';
     }
+    std::cout << '\n';
+  }
 }
 
 void self_completion(int **m, std::size_t rows, std::size_t cols) {
-    if (m == nullptr) return;
-    for (int i{}; i < rows; i++) {
-        std::cout << "Row " << i + 1 << ": ";
-        for (int j{}; j < cols; j++) if (!(std::cin >> m[i][j])) std::cin.clear();
-    }
+  if (m == nullptr)
+    return;
+  for (int i{}; i < rows; i++) {
+    std::cout << "Row " << i + 1 << ": ";
+    for (int j{}; j < cols; j++)
+      if (!(std::cin >> m[i][j]))
+        std::cin.clear();
+  }
 }
